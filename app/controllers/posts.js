5e86c0a5160addc023c3583bd21bd71086e94af3
suzannekozina <kjartan.kozina13@gmail.com>
2016-11-01 19:24:48 -0500

    add blog

---
+++ app/controllers/posts.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+// export default Ember.Controller.extend({
+// });
+export default Ember.ArrayController.extend({
+  sortProperties: ['timestamp'],
+  sortAscending: false, // sorts post by timestamp
+  actions: {
+    publishPost: function() {
+      var newPost = this.store.createRecord('post', {
+        title: this.get('title'),
+        body: this.get('body'),
+        timestamp: new Date().getTime()
+      });
+      newPost.save();
+    }
+  }
+});
